3f10d9e9ea200dacbdb6ab0e6d2ed85c308b4bbc
AdamMEI <aimeilicke@gmail.com>
2023-05-17 19:57:47 -0700

    Fixed model name and visualization

--- tests.py
+++ tests.py
@@ -6,42 +6,45 @@ Created on Wed May 17 17:38:22 2023
 @author: adamthepig
 """
 
-import population_playground
+import model
 import numpy as np
 
 def spawnTest():
+    """
+    Tests the random spawning of prey, predators, and plants.
+    """
     prey, preyMask, predator, predatorMask, plants, plantMask = \
-        population_playground.initialize()
-    population_playground.visualize(preyMask, predatorMask, plantMask)
+        model.initialize()
+    model.visualize(preyMask, predatorMask, plantMask, plants)
 
 def testFeed(y, x):
     prey, preyMask, predators, predatorMask, plants, plantMask = \
-        population_playground.initialize()
-    prey[y, x, 0] = population_playground.PREY_START_ENERGY
-    prey[y, x, 1] = population_playground.PREY_REPRODUCTION_START_TIME
+        model.initialize()
+    prey[y, x, 0] = model.PREY_START_ENERGY
+    prey[y, x, 1] = model.PREY_REPRODUCTION_START_TIME
     preyMask[y, x] = True
     predators[y, x, 0] = 15
-    predators[y, x, 1] = population_playground.PREDATOR_REPRODUCTION_START_TIME
+    predators[y, x, 1] = model.PREDATOR_REPRODUCTION_START_TIME
     predatorMask[y, x] = True
-    population_playground.feed(prey, preyMask, predators, predatorMask,
+    model.feed(prey, preyMask, predators, predatorMask,
                                plants, plantMask)
     return (prey, preyMask, predators, predatorMask, plants, plantMask)
 
 def stunTest():
     y = 4
     x = 4
-    population_playground.STUN_CHANCE = 1.0
-    population_playground.PREY_KILL_CHANCE = 0.0
+    model.STUN_CHANCE = 1.0
+    model.PREY_KILL_CHANCE = 0.0
     prey, preyMask, predators, predatorMask, plants, plantMask = testFeed(y, x)
     assert(preyMask[y, x])
     assert(predatorMask[y, x])
-    assert(predators[y, x, 2] == population_playground.STUN_TIME)
+    assert(predators[y, x, 2] == model.STUN_TIME)
 
 def killTest():
     y = 4
     x = 4
-    population_playground.STUN_CHANCE = 0.0
-    population_playground.PREY_KILL_CHANCE = 1.0
+    model.STUN_CHANCE = 0.0
+    model.PREY_KILL_CHANCE = 1.0
     prey, preyMask, predators, predatorMask, plants, plantMask = testFeed(y, x)
     assert(preyMask[y, x])
     assert(not predatorMask[y, x])
@@ -50,57 +53,57 @@ def killTest():
 def eatTest():
     y = 4
     x = 4
-    population_playground.STUN_CHANCE = 0.0
-    population_playground.PREY_KILL_CHANCE = 0.0
+    model.STUN_CHANCE = 0.0
+    model.PREY_KILL_CHANCE = 0.0
     prey, preyMask, predators, predatorMask, plants, plantMask = testFeed(y, x)
     assert(not preyMask[y, x])
     assert(predatorMask[y, x])
     assert(np.array_equal(prey[y, x], np.array([0, 0])))
     assert(predators[y, x, 0] == min( \
-        15 + population_playground.PREDATOR_EAT_ENERGY,
-        population_playground.PREDATOR_MAX_ENERGY))
+        15 + model.PREDATOR_EAT_ENERGY,
+        model.PREDATOR_MAX_ENERGY))
 
 def eatPlantTest():
     y = 4
     x = 4
-    population_playground.STUN_CHANCE = 0.0
-    population_playground.PREY_KILL_CHANCE = 0.0
+    model.STUN_CHANCE = 0.0
+    model.PREY_KILL_CHANCE = 0.0
     prey, preyMask, predators, predatorMask, plants, plantMask = \
-        population_playground.initialize()
+        model.initialize()
     prey[y, x, 0] = 15
-    prey[y, x, 1] = population_playground.PREY_REPRODUCTION_START_TIME
+    prey[y, x, 1] = model.PREY_REPRODUCTION_START_TIME
     preyMask[y, x] = True
     plantMask[y, x] = True
-    population_playground.feed(prey, preyMask, predators, predatorMask,
+    model.feed(prey, preyMask, predators, predatorMask,
                                plants, plantMask)
     assert(preyMask[y, x])
     assert(plantMask[y, x])
-    assert(plants[y, x] == population_playground.PLANT_REGROWTH_TIME)
+    assert(plants[y, x] == model.PLANT_REGROWTH_TIME)
     assert(prey[y, x, 0] == min( \
-        15 + population_playground.PREY_EAT_ENERGY,
-        population_playground.PREY_MAX_ENERGY))
+        15 + model.PREY_EAT_ENERGY,
+        model.PREY_MAX_ENERGY))
 
 def eatUngrownPlantTest():
     y = 4
     x = 4
-    population_playground.STUN_CHANCE = 0.0
-    population_playground.PREY_KILL_CHANCE = 0.0
+    model.STUN_CHANCE = 0.0
+    model.PREY_KILL_CHANCE = 0.0
     prey, preyMask, predators, predatorMask, plants, plantMask = \
-        population_playground.initialize()
+        model.initialize()
     prey[y, x, 0] = 15
-    prey[y, x, 1] = population_playground.PREY_REPRODUCTION_START_TIME
+    prey[y, x, 1] = model.PREY_REPRODUCTION_START_TIME
     preyMask[y, x] = True
     plantMask[y, x] = True
-    plants[y, x] = population_playground.PLANT_REGROWTH_TIME
-    population_playground.feed(prey, preyMask, predators, predatorMask,
+    plants[y, x] = model.PLANT_REGROWTH_TIME
+    model.feed(prey, preyMask, predators, predatorMask,
                                plants, plantMask)
     assert(preyMask[y, x])
     assert(plantMask[y, x])
-    assert(plants[y, x] == population_playground.PLANT_REGROWTH_TIME - 1)
+    assert(plants[y, x] == model.PLANT_REGROWTH_TIME - 1)
     assert(prey[y, x, 0] == 15)
     
     
-
+spawnTest()
 stunTest()
 killTest()
 eatTest()
